a00e0313a9ac7bb4a0790e2276b694999bf5ccfc
Brendan Studds <brendan.studds@gmail.com>
2018-07-24 14:13:48 +1000

    Moving bash conf around

--- bash/conf/aliases.sh
+++ bash/conf/aliases.sh
@@ -1,4 +1,4 @@
-# List aliases
+#!/bin/bash
 alias ll='/usr/local/bin/gls -alF --color=auto --group-directories-first'
 alias la='/usr/local/bin/gls -A --color=auto --group-directories-first'
 
@@ -6,12 +6,26 @@ alias ls='/usr/local/bin/gls -h --color=auto --group-directories-first'
 alias dir='dir --color=auto'
 alias vdir='vdir --color=auto'
 
+
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
+
 
 function cdiff() {
   colordiff -w -W 165 $@ | less -R;
 }
 
-alias dcr='docker-compose run --rm'
+if [ -n "$(command -v pacman)" ]; then
+  alias pacman="pacman --color always"
+fi
+
+
+if [ -n "$(command -v docker-compose)" ]; then
+  alias dcr='docker-compose run --rm'
+
+  dcrf() {
+    docker-compose -f "$1" run ${@:2}
+  }
+
+fi
